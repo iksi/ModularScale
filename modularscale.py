0d6279364bce7667f0c04ff976f738065fb15d58
iksi <mail@iksi.cc>
2017-02-21 16:38:48 +0100

    rem option

--- modularscale.py
+++ modularscale.py
@@ -2,34 +2,35 @@ from __future__ import division
 import math
 
 ratios = {
-  'doubleOctave': 4 / 1,               # 4
-  'majorTwelfth': 3 / 1,               # 3
-  'majorEleventh': 8 / 3,              # 2.667
-  'majorTenth': 5 / 2,                 # 2.5
-  'octave': 2 / 1,                     # 2
-  'majorSeventh': 15 / 8,              # 1.875
-  'minorSeventh': 16 / 9,              # 1.778
-  'majorSixth': 5 / 3,                 # 1.667
-  'golden': 1 / 2 + math.sqrt(5) / 2,  # 1.618
-  'minorSixth': 8 / 5,                 # 1.6
-  'fifth': 3 / 2,                      # 1.5
-  'augmentedFourth': math.sqrt(2) / 1, # 1.414
-  'fourth': 4 / 3,                     # 1.333
-  'majorThird': 5 / 4,                 # 1.25
-  'minorThird': 6 / 5,                 # 1.2
-  'majorSecond': 9 / 8,                # 1.125
-  'minorSecond': 16 / 15,              # 1.067
+  'doubleOctave': 4 / 1,                # 4
+  'majorTwelfth': 3 / 1,                # 3
+  'majorEleventh': 8 / 3,               # 2.667
+  'majorTenth': 5 / 2,                  # 2.5
+  'octave': 2 / 1,                      # 2
+  'majorSeventh': 15 / 8,               # 1.875
+  'minorSeventh': 16 / 9,               # 1.778
+  'majorSixth': 5 / 3,                  # 1.667
+  'golden': 1 / 2 + math.sqrt(5) / 2,   # 1.618
+  'minorSixth': 8 / 5,                  # 1.6
+  'fifth': 3 / 2,                       # 1.5
+  'augmentedFourth': math.sqrt(2) / 1,  # 1.414
+  'fourth': 4 / 3,                      # 1.333
+  'majorThird': 5 / 4,                  # 1.25
+  'minorThird': 6 / 5,                  # 1.2
+  'majorSecond': 9 / 8,                 # 1.125
+  'minorSecond': 16 / 15,               # 1.067
 }
 
-base = 20
-ratio = ratios['octave']
+base = 16
+ratio = ratios['majorThird']
 steps = 7
-rounded = True
+rem = True
 
 for step in range(0, steps):
   number = base * math.pow(ratio, step)
-  output = '%d: %f'
-  if rounded == True:
-    number = math.floor(number)
-    output = '%d: %f'
-  print output % (step + 1, number)
+  number = int(number)
+  if rem:
+    number = number / 16
+    if number.is_integer():
+      number = int(number)
+  print '{}: {}'.format(step + 1, number)
